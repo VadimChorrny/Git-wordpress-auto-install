f3327edbf0243579f3d6e49680b023fece623f2b
Bogdan <71147719+bkomarow1990@users.noreply.github.com>
2020-10-30 14:22:09 +0200

    Added func create db, and ip replace

--- worpdress_autoinstall.sh
+++ worpdress_autoinstall.sh
@@ -1,5 +1,10 @@
-#!/bin/bash
 function installWordpress(){
+    echo "NAME OF DB "
+    read -e namedb
+    echo "USER OF DB "
+    read -e userdb
+    echo "PASS OF DB "
+    read -s passdb
     sudo yum -y update httpd
     sudo yum -y install httpd
     sudo firewall-cmd --permanent --add-service=http
@@ -10,21 +15,36 @@ function installWordpress(){
     sudo yum -y install mariadb-server
     sudo systemctl start mariadb
     sudo systemctl enable mariadb
-    sudo mysql_secure_installation
-    yum install epel-release yum-utils -y
-    yum install http://rpms.remirepo.net/enterprise/remi-release-7.rpm
-    yum install php php-common php-opcache php-mcrypt php-cli php-gd php-curl php-mysql -y
-    yum -y install phpmyadmin
-    yum -y install rsync
-    cd /var/www/ || exit
-    curl https://uk.wordpress.org/latest-uk.tar.gz > wordpress.tar.gz
-    tar xzf wordpress.tar.gz
-    rsync -avP wordpress/ /var/www/html/
-    sudo mysql -u root -e "CREATE DATABASE wordpress"
+    echo "PASS OF DB "
+    read -e INIT_PASSWORD
+    sudo mysql -u root -p$INIT_PASSWORD mysql -e 
+    sudo yum install epel-release yum-utils -y
+    sudo yum install http://rpms.remirepo.net/enterprise/remi-release-7.rpm
+    sudo yum install php php-common php-opcache php-mcrypt php-cli php-gd php-curl php-mysql -y
+    sudo yum -y install phpmyadmin
+    sudo yum -y install rsync
+    cd /var/www
+    sudo curl https://uk.wordpress.org/latest-uk.tar.gz > wordpress.tar.gz
+    sudo tar xzf wordpress.tar.gz
+    sudo chmod 777 /var/www/html -R
+    sudo rsync -avP wordpress /var/www/html/
+    sudo mv /var/www/html/wp-config-sample.php /var/www/html/wp-config.php 
+    read -r -p "ENTER IP FROM WHICH YOU WILL ENTER IN ADMIN PANEL" ip
+    ip_sample=$(grep -E -o -m 1 "([0-9]{1,3}[.]){3}[0-9]{1,3}" /etc/httpd/conf.d/phpMyAdmin.conf | head -1)
+    sed -i "s/$ip_sample/$ip/g" /etc/httpd/conf.d/phpMyAdmin.conf
+    read -r -p "DBNAME: " dbname
+    read -r -p "USERNAME: " username
+    read -r -p "DATABASE USER PASSWORD: " userpassword
+    sudo mysql -u root -p -e "CREATE DATABASE $dbname"
+    sudo mysql -u root -p -e "CREATE USER '$username' IDENTIFIED BY '$userpassword'"
+    sudo mysql -u root -p -e "GRANT ALL PRIVILEGES ON $dbname TO '$username'"
+    sed -i "s/database_name_here/$dbname/g" /home/wordpress/wp-config.php
+    sed -i "s/username_here/$username/g" /home/wordpress/wp-config.php
+    sed -i "s/password_here/$userpassword/g" /home/wordpress/wp-config.php
+    systemctl restart httpd
 }
 read -p "Do you want to install Wordpress? " answer
 if [[ "$answer" == "y" ]]; then
     installWordpress;
 elif [[ "$answer" == "n" ]]; then
     exit
-fi
