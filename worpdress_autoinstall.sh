7709c3d72118ebf503493f0ba590bb59c5a4bcf4
Vadim <65212516+VadimChorrny@users.noreply.github.com>
2020-11-02 20:40:00 +0200

    Update worpdress_autoinstall.sh

--- worpdress_autoinstall.sh
+++ worpdress_autoinstall.sh
@@ -7,15 +7,18 @@ function installWordpress(){
     read -s passdb
     sudo yum -y update httpd
     sudo yum -y install httpd
+    # Apache
     sudo firewall-cmd --permanent --add-service=http
     sudo firewall-cmd --permanent --add-service=https
     sudo firewall-cmd --reload
     sudo systemctl start httpd
     sudo systemctl enable httpd
+    # MariaDB
     sudo yum -y install mariadb-server
     sudo systemctl start mariadb
     sudo systemctl enable mariadb
     echo "PASS OF DB "
+    # pass for mysql
     read -e INIT_PASSWORD
     sudo mysql -u root -p$INIT_PASSWORD mysql -e 
     sudo yum install epel-release yum-utils -y
@@ -24,25 +27,29 @@ function installWordpress(){
     sudo yum -y install phpmyadmin
     sudo yum -y install rsync
     cd /var/www
+    # WordPress
     sudo curl https://uk.wordpress.org/latest-uk.tar.gz > wordpress.tar.gz
     sudo tar xzf wordpress.tar.gz
     sudo chmod 777 /var/www/html -R
     sudo rsync -avP wordpress /var/www/html/
     sudo mv /var/www/html/wp-config-sample.php /var/www/html/wp-config.php 
-    read -r -p "ENTER IP FROM WHICH YOU WILL ENTER IN ADMIN PANEL" ip
+    read -r -p "Enter ip" ip
     ip_sample=$(grep -E -o -m 1 "([0-9]{1,3}[.]){3}[0-9]{1,3}" /etc/httpd/conf.d/phpMyAdmin.conf | head -1)
     sed -i "s/$ip_sample/$ip/g" /etc/httpd/conf.d/phpMyAdmin.conf
-    read -r -p "DBNAME: " dbname
-    read -r -p "USERNAME: " username
-    read -r -p "DATABASE USER PASSWORD: " userpassword
+    read -r -p "Name DB: " dbname
+    read -r -p "Username: " username
+    read -r -p "Pass DB: " userpassword
     sudo mysql -u root -p -e "CREATE DATABASE $dbname"
     sudo mysql -u root -p -e "CREATE USER '$username' IDENTIFIED BY '$userpassword'"
     sudo mysql -u root -p -e "GRANT ALL PRIVILEGES ON $dbname TO '$username'"
+    # Dell
     sed -i "s/database_name_here/$dbname/g" /home/wordpress/wp-config.php
     sed -i "s/username_here/$username/g" /home/wordpress/wp-config.php
     sed -i "s/password_here/$userpassword/g" /home/wordpress/wp-config.php
+    # restart Apache
     systemctl restart httpd
 }
+
 read -p "Do you want to install Wordpress? " answer
 if [[ "$answer" == "y" ]]; then
     installWordpress;
